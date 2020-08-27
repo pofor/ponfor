821efd4ebe480ce05388bbceadde04ff67c24d69
pofor <pontus.forslermott@elev.ga.ntig.se>
2020-08-27 09:18:13 +0200

    Hello Niklas

--- helloworld/Helloworld/Helloworld/Program.cs
+++ helloworld/Helloworld/Helloworld/Program.cs
@@ -6,7 +6,7 @@ namespace Helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Niklas!");
         }
     }
 }
